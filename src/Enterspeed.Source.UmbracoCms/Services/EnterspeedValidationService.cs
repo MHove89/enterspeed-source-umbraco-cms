ce74424917cb73291c799a0b1ee5923ab93eac86
MHove89 <mathiashove02@gmail.com>
2022-12-23 12:34:35 -0600

    #2031 : Added property validation

---
+++ src/Enterspeed.Source.UmbracoCms/Services/EnterspeedValidationService.cs
@@ -0,0 +1,35 @@
+﻿using Enterspeed.Source.Sdk.Api.Models.Properties;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Enterspeed.Source.UmbracoCms.Services
+{
+    public class EnterspeedValidationService : IEnterspeedValidationService
+    {
+        private readonly ILogger<EnterspeedValidationService> _logger;
+
+        public EnterspeedValidationService(ILogger<EnterspeedValidationService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogValidationError(IEnterspeedProperty property)
+        {
+            if (!property.Validation.IsValid)
+            {
+                foreach (var error in property.Validation.Errors)
+                {
+                    _logger.LogError(error);
+                }
+            }
+        }
+
+        public void LogValidationErrors(IEnumerable<IEnterspeedProperty> enterspeedProperties)
+        {
+            foreach (var property in enterspeedProperties)
+            {
+                LogValidationError(property);
+            }
+        }
+    }
+}
